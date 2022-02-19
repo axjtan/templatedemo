239cb6bc7b004173ec1916a16ea3d80bab448339
tanxinjialan <tan.xin.ji.alan@gmail.com>
2022-02-19 10:49:13 +0800

    Initial commit

---
+++ src/main/java/com/axjtan/templatedemo/TemplatedemoApplication.java
@@ -0,0 +1,13 @@
+package com.axjtan.templatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axjtan/templatedemo/TemplatedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axjtan.templatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplatedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
